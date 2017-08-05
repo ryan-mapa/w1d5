19b03ae7c1ab6084402d6eac3b620cb025820231
App Academy Student <student@appacademy.io>
2017-08-04 17:05:59 -0700

    write all but find_path on travails

--- skeleton/lib/knighttravails.rb
+++ skeleton/lib/knighttravails.rb
@@ -9,18 +9,38 @@ class KnightPathFinder
   end
 
   def build_move_tree
+    node1 = PolyTreeNode.new(@start)
+    q = [node1]
+    until q.empty?
+      first = q.shift
+      possible = new_move_positions(first.value)
+      possible.each do |pos|
+        current = PolyTreeNode.new(pos)
+        current.parent = first
+        q << current
+      end
+    end
+  end
 
+  def on_board?(pos)
+    x , y = pos
+    return true if x.between?(0,7) && y.between?(0,7)
+
+    false
   end
 
   def self.valid_moves(pos)
-
-
+    #just make sure x and y are between 0, 7
+    x, y = pos
+    possible_moves = [[x+2,y+1],[x+2,y-1],[x+1,y+2],[x+1,y-2],
+                      [x-1,y+2],[x-1,y-2],[x-2,y+1],[x-2,y-1]]
+    possible_moves.select {|move| on_board?(move)}
   end
 
   def new_move_positions(pos)
     possible = KnightPathFinder.valid_moves(pos)
     possible.reject! {|pos| @visited_positions.include?(pos)}
-    @visited_positions += possible
+    @visited_positions += possible #look at this later
     possible
   end
 
